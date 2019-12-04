3febda65cc0bb31d6ba53e73c6cf9436c7bc104a
Sanjeev Namjoshi <snamjoshi87@utexas.edu>
2019-12-04 15:33:55 -0600

    Debugging

--- setup.py
+++ setup.py
@@ -8,7 +8,7 @@ setup(
     author_email      = 'sanjeev@yonder.co',
     packages          = ['objectDetectionD3MWrapper'],
     install_requires  = ['numpy>=1.15.4,<=1.17.3',
-                         'object_detection_retinanet @ git+https://github.com/NewKnowledge/object-detection-retinanet@2cdbf6e968a4df90f0bef1673897f2d9cf7ea230#egg=object_detection_retinanet'],                        
+                         'object_detection_retinanet @ git+https://github.com/NewKnowledge/object-detection-retinanet@225242a2210db878001802601315ad900c7a4229#egg=object_detection_retinanet'],                        
     entry_points      = {
         'd3m.primitives': [
             'object_detection.retinanet_convolutional_neural_network = objectDetectionD3MWrapper:ObjectDetectionRNPrimitive'
