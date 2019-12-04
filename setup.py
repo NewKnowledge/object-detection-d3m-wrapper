be2fb37ce8c36315431aa5a723fffb2e0017e119
Sanjeev Namjoshi <snamjoshi87@utexas.edu>
2019-12-04 16:49:23 -0600

    Reverting to previous version pointing to has without TF2 compatibiility

--- setup.py
+++ setup.py
@@ -8,7 +8,7 @@ setup(
     author_email      = 'sanjeev@yonder.co',
     packages          = ['objectDetectionD3MWrapper'],
     install_requires  = ['numpy>=1.15.4,<=1.17.3',
-                         'object_detection_retinanet @ git+https://github.com/NewKnowledge/object-detection-retinanet@13f779fce21418cbe7dc9fc9fc4f25c601181f6e#egg=object_detection_retinanet'],                        
+                         'object_detection_retinanet @ git+https://github.com/NewKnowledge/object-detection-retinanet@2378bba0981970e58bb5ee6bbc095cdc9f7e65e0#egg=object_detection_retinanet'],                        
     entry_points      = {
         'd3m.primitives': [
             'object_detection.retinanet_convolutional_neural_network = objectDetectionD3MWrapper:ObjectDetectionRNPrimitive'
