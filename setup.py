7b3d7abe251b43d38a19a63ac58e82e22ac89d8f
Sanjeev Namjoshi <snamjoshi87@utexas.edu>
2019-12-05 10:43:08 -0600

    Changed hash in setup

--- setup.py
+++ setup.py
@@ -8,7 +8,7 @@ setup(
     author_email      = 'sanjeev@yonder.co',
     packages          = ['objectDetectionD3MWrapper'],
     install_requires  = ['numpy>=1.15.4,<=1.17.3',
-                         'object_detection_retinanet @ git+https://github.com/NewKnowledge/object-detection-retinanet@2378bba0981970e58bb5ee6bbc095cdc9f7e65e0#egg=object_detection_retinanet'],                        
+                         'object_detection_retinanet @ git+https://github.com/NewKnowledge/object-detection-retinanet@83de0b72fb38efc20c9e71f2888907adbd02ca31#egg=object_detection_retinanet'],                        
     entry_points      = {
         'd3m.primitives': [
             'object_detection.retinanet_convolutional_neural_network = objectDetectionD3MWrapper:ObjectDetectionRNPrimitive'
